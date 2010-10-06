3ad5337ce9fa7c884250459ac98caa8118fc0da0
AndyLenox <zmobie@gmail.com>
2010-10-06 17:02:47 -0400

    Initial Commit

---
+++ Build/freeglut/CMakeFiles/freeglut_static.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_callbacks.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_callbacks.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_cursor.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_cursor.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_display.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_display.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_ext.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_ext.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_font.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_font.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_font_data.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_font_data.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_gamemode.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_gamemode.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_geometry.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_geometry.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_glutfont_definitions.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_glutfont_definitions.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_init.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_init.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_input_devices.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_input_devices.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_joystick.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_joystick.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_main.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_main.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_menu.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_menu.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_misc.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_misc.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_overlay.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_overlay.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_spaceball.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_spaceball.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_state.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_state.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_stroke_mono_roman.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_stroke_mono_roman.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_stroke_roman.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_stroke_roman.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_structure.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_structure.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_teapot.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_teapot.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_videoresize.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_videoresize.o"
+  "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/freeglut/freeglut_window.c" "/Users/Andy/Documents/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/freeglut_window.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "FREEGLUT_EXPORTS"
+  "FREEGLUT_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  "__unix__"
+  "HAVE_FCNTL_H"
+  "HAVE_GETTIMEOFDAY"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
